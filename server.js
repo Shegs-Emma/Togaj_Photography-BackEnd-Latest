402e9d6b46f1e98b455837c5039ad44496cc26b8
EmmyM_ighty <remi.emma04@gmail.com>
2020-12-23 13:20:45 +0100

    Test 4

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ if (process.env.NODE_ENV === 'production') {
 
 
     app.get('*', function (req, res) {
-        res.sendFile(path.join(__dirname, './client/build', 'index.html'))
+        // res.sendFile(path.join(__dirname, './client/build', 'index.html'))
+        res.sendFile(path.join(__dirname, './public', 'index.html'))
     })
 }
 
