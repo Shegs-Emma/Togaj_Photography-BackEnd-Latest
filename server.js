f7c3684dc1bdb43170bb1aec5bb0e6c6f133d9da
EmmyM_ighty <remi.emma04@gmail.com>
2020-12-23 13:38:35 +0100

    Test 5

--- server.js
+++ server.js
@@ -6,15 +6,18 @@ const   http    = require('http'),
 
 if (process.env.NODE_ENV === 'production') {
     //Serve any static files
-    // app.use(express.static(path.join(__dirname, './client/build')));
-    app.use(express.static('public'))
+    app.use(express.static(path.join(__dirname, '/../client/build')));
+    // app.use(express.static('public'))
 
 
     app.get('*', function (req, res) {
         // res.sendFile(path.join(__dirname, './client/build', 'index.html'))
-        res.sendFile(path.join(__dirname, './public', 'index.html'))
+        app.use(express.static(path.join(__dirname, '/../client/build')));
+
+        // res.sendFile(path.join(__dirname, './public', 'index.html'))
     })
 }
+console.log("The dir is " + __dirname);
 
 app.set('port', process.env.PORT || 3001);
 
